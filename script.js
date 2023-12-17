094c0a72933cc28ea9ae5ec12f6042280f10050a
stokato <demyanovpaul@yandex.ru>
2023-12-18 02:47:49 +0300

    Добавил решение 2 задания уровня элементарно

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-let title = '';
-let screens = '';
-let screenPrice = '';
-let rollback = '';
-let fullPrice = '';
-let adaptive = '';
+const title = 'Название проекта';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 123;
+const rollback = Math.trunc(Math.random() * 100 + 1);
+const fullPrice = Infinity;
+const adaptive = true;
 
-alert('aloha');
-console.log('сообщение с любым текстом');
+//Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+//Вывести в консоль длину строки из переменной screens
+console.log(screens.length);
+
+/*
+Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани”
+и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+*/
+console.log(`${screenPrice} рублей`);
+console.log(`${screenPrice / 89,7} долларов`);
+console.log(`${screenPrice / 2,42} гривен`);
+console.log(`${screenPrice / 12,61} юани`);
+
+console.log(`${rollback} рублей`);
+console.log(`${rollback / 89,7} долларов`);
+console.log(`${rollback / 2,42} гривен`);
+console.log(`${rollback / 12,61} юани`);
+
+//Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(screens.toLowerCase().split(''));
+
+//Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+console.log(fullPrice * (rollback / 100));
