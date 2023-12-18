fb4955ea5853ded288e8ee728df7e2bc8ae318d6
stokato <demyanovpaul@yandex.ru>
2023-12-18 12:44:48 +0300

    Переношу в новую репу для расширенных заданий

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-const title = 'Название проекта';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 123;
-const rollback = Math.trunc(Math.random() * 100 + 1);
-const fullPrice = Infinity;
-const adaptive = true;
-
-//Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-//Вывести в консоль длину строки из переменной screens
-console.log(screens.length);
+//Создать переменную num со значением 266219 (тип данных число)
+let num = 266219;
+let result = 1;
 
 /*
-Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани”
-и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+Вывести в консоль произведение (умножение) цифр этого числа
+Например: число 123, при помощи javaScript получить каждую цифру ( 1, 2, 3 ) и перемножить их. Правильно использовать цикл или методы перебора.
 */
-console.log(`${screenPrice} рублей`);
-console.log(`${screenPrice / 89,7} долларов`);
-console.log(`${screenPrice / 2,42} гривен`);
-console.log(`${screenPrice / 12,61} юани`);
+while(num > 0) {
+    result *= num % 10;
+    num = Math.floor(num / 10);
+}
+console.log(result);
 
-console.log(`${rollback} рублей`);
-console.log(`${rollback / 89,7} долларов`);
-console.log(`${rollback / 2,42} гривен`);
-console.log(`${rollback / 12,61} юани`);
+//Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+result **= 3;
+console.log(result);
 
-//Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-console.log(screens.toLowerCase().split(''));
-
-//Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-console.log(fullPrice * (rollback / 100));
+//Вывести в консоль первые 2 цифры полученного числа
+console.log(`${result}`.substr(0, 2));
