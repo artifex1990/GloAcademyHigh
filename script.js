6755218e7551194571b2d40f718144d0b8bfe889
stokato <demyanovpaul@yandex.ru>
2024-01-05 23:19:54 +0300

    Задание 16

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class First {
+    hello() {
+        console.log('Привет я метод родителя!');
+    }
+}
+
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+}
+
+new Second().hello();
