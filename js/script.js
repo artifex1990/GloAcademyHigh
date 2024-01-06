c40ee1810f5a0bd8db80fa5a921ba7ad0e24060a
Pavel Demyanov <demyanovpaul@yandex.ru>
2024-01-07 00:34:19 +0300

    Задание 17 - исправил замечание с валидацией

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 'use strict';
 
-const firstname = document.querySelector('input[name=firstname]');
-const surname = document.querySelector('input[name=surname]');
-const age = document.querySelector('input[name=age]');
-const position = document.querySelector('select[name=position]');
-const organization = document.querySelector('input[name=organization]');
-const category = document.querySelector('input[name=category]');
-const presenceOfChildren = document.querySelector('input[name=presenceOfChildren]');
-const dateOfEmployment = document.querySelector('input[name=dateOfEmployment]');
+const firstnameInput = document.querySelector('input[name=firstname]');
+const surnameInput = document.querySelector('input[name=surname]');
+const ageInput = document.querySelector('input[name=age]');
+const positionSelect = document.querySelector('select[name=position]');
+const organizationInput = document.querySelector('input[name=organization]');
+const categoryInput = document.querySelector('input[name=category]');
+const presenceOfChildrenInput = document.querySelector('input[name=presenceOfChildren]');
+const dateOfEmploymentInput = document.querySelector('input[name=dateOfEmployment]');
 const saveBtn = document.querySelector('button[type=submit]');
 const app = document.getElementById('app');
+
+const forEventListener = [
+    firstnameInput,
+    surnameInput,
+    organizationInput,
+    ageInput,
+    categoryInput
+];
+
+dateOfEmploymentInput.addEventListener('change', event => {
+    event.target.checkValidity();
+    event.target.setCustomValidity('');
+    event.target.removeAttribute('required');
+});
+forEventListener.forEach(el => el.addEventListener('input', event => {
+    event.target.checkValidity();
+    event.target.setCustomValidity('');
+    event.target.removeAttribute('required');
+}));
 
 const getData = (key) => JSON.parse(localStorage.getItem(key)) || [];
 const setData = (key, object) => localStorage.setItem(key, JSON.stringify(object));
@@ -33,53 +52,55 @@ const render = () => {
 };
 
 const clearForm = () => {
-    firstname.value = '';
-    surname.value = '';
-    age.value = '';
-    organization.value = '';
-    category.value = '';
-    dateOfEmployment.value = '';
-    position.value = 'Слесарь';
-    presenceOfChildren.checked = false;
+    const tempClearAttributeRequired = forEventListener;
+
+    tempClearAttributeRequired.push(dateOfEmploymentInput);
+    tempClearAttributeRequired.forEach(el => {
+        el.checkValidity();
+        el.setCustomValidity('');
+        el.removeAttribute('required');
+    });
+
+    firstnameInput.value = '';
+    surnameInput.value = '';
+    organizationInput.value = '';
+    dateOfEmploymentInput.value = '';
+    positionSelect.value = 'Слесарь';
+    presenceOfChildrenInput.checked = false;
+    ageInput.value = 16;
+    categoryInput.value = 1;
 };
 
 saveBtn.addEventListener('click', (event) => {
     const employers = getData('employers');
     const fieldsForCheckOnEmpty = [
-        firstname.value,
-        surname.value,
-        age.value,
-        organization.value,
-        category.value,
-        dateOfEmployment.value
+        firstnameInput,
+        surnameInput,
+        organizationInput,
+        dateOfEmploymentInput
     ];
     const fieldsForCheckOnNumber = [
-        age.value,
-        category.value
+        ageInput,
+        categoryInput
     ];
-
+    const isEmpty = CheckForm.isEmpty(fieldsForCheckOnEmpty);
+    const isNotNumber = !CheckForm.isNumber(fieldsForCheckOnNumber);
+    const isNotAge = !CheckForm.isAge(ageInput);
+    const isNotCategory = !CheckForm.isCategory(categoryInput);
     let employee = '';
 
     event.preventDefault();
 
-    if (CheckForm.isEmpty(fieldsForCheckOnEmpty)) {
-        alert('Заполните все поля!');
-
+    if (isEmpty || isNotNumber || isNotAge || isNotCategory) {
         return;
     }
 
-    if (CheckForm.isNumber(fieldsForCheckOnNumber)) {
-        alert('Для полей возраст, или категории указан не числовой формат!');
-
-        return;
-    }
-
-    switch(position.options[position.selectedIndex].value) {
+    switch(positionSelect.options[positionSelect.selectedIndex].value) {
         case 'Слесарь':
-            employee = new Plumber(firstname.value, surname.value, age.value, organization.value, category.value, presenceOfChildren.checked, dateOfEmployment.value);
+            employee = new Plumber(firstnameInput.value, surnameInput.value, ageInput.value, organizationInput.value, categoryInput.value, presenceOfChildrenInput.checked, dateOfEmploymentInput.value);
             break;
         case 'Водитель':
-            employee = new Driver(firstname.value, surname.value, age.value, organization.value, category.value, presenceOfChildren.checked, dateOfEmployment.value);
+            employee = new Driver(firstnameInput.value, surnameInput.value, ageInput.value, organizationInput.value, categoryInput.value, presenceOfChildrenInput.checked, dateOfEmploymentInput.value);
             break;    
     }
 
@@ -90,7 +111,16 @@ saveBtn.addEventListener('click', (event) => {
 });
 
 class Employee {
-    constructor(firstname, surname, age, organization, category, presenceOfChildren, dateOfEmployment, id = 0) {
+    constructor(
+        firstname,
+        surname,
+        age,
+        organization,
+        category,
+        presenceOfChildren,
+        dateOfEmployment,
+        id = 0
+    ) {
         this._id = !id ? new Date().getTime() : id;
         this._firstname = firstname;
         this._surname = surname;
@@ -108,73 +138,91 @@ class Employee {
     get firstname() {
         return this._firstname;
     }
+    
+    set firstname(firstname) {
+        this._firstname = firstname;
+    }
 
     get surname() {
         return this._surname;
-    }
-
-    get age() {
-        return this._age;
-    }
-
-    get organization() {
-        return this._organization;
-    }
-
-    get category() {
-        return this._category;
-    }
-
-    get presenceOfChildren() {
-        return this._presenceOfChildren;
-    }
-
-    get dateOfEmployment() {
-        return this._dateOfEmployment;
-    }
-
-    get position() {
-        return this._position;
-    }
-
-    set firstname(firstname) {
-        this._firstname = firstname;
     }
 
     set surname(surname) {
         this._surname = surname;
     }
 
+    get age() {
+        return this._age;
+    }
+
     set age(age) {
         this._age = age;
+    }
+
+    get organization() {
+        return this._organization;
     }
 
     set organization(organization) {
         this._organization = organization;
     }
 
+    get category() {
+        return this._category;
+    }
+
     set category(category) {
         this._category = category;
+    }
+
+    get presenceOfChildren() {
+        return this._presenceOfChildren;
     }
 
     set presenceOfChildren(presenceOfChildren) {
         this._presenceOfChildren = presenceOfChildren;
     }
 
+    get dateOfEmployment() {
+        return this._dateOfEmployment;
+    }
+
     set dateOfEmployment(dateOfEmployment) {
         this._dateOfEmployment = dateOfEmployment;
+    }
+
+    get position() {
+        return this._position;
     }
 }
 
 class Plumber extends Employee {
-    constructor(firstname, surname, age, organization, category, presenceOfChildren, dateOfEmployment, id = 0) {
+    constructor(
+        firstname,
+        surname,
+        age,
+        organization,
+        category,
+        presenceOfChildren,
+        dateOfEmployment,
+        id = 0
+    ) {
         super(firstname, surname, age, organization, category, presenceOfChildren, dateOfEmployment, id);
         this._position = 'Слесарь';
     }
 }
 
 class Driver extends Employee {
-    constructor(firstname, surname, age, organization, category, presenceOfChildren, dateOfEmployment, id = 0) {
+    constructor(
+        firstname,
+        surname,
+        age,
+        organization,
+        category,
+        presenceOfChildren,
+        dateOfEmployment,
+        id = 0
+    ) {
         super(firstname, surname, age, organization, category, presenceOfChildren, dateOfEmployment, id);
         this._position = 'Водитель';
     }
@@ -237,19 +285,71 @@ class WorkDOM {
 
 class CheckForm {
     static isEmpty(fields) {
+        let isEmpty = false;
+
         for (let el of fields) {
-            if (!el.trim()) return true;
+            if (!el.value.trim()) {
+                el.setAttribute('required', '');
+                el.setCustomValidity('Поле не должно быть пустым!');
+                el.checkValidity();
+
+                isEmpty = true;
+            } else {
+                el.setCustomValidity('');
+                el.removeAttribute('required');
+            }
         }
 
-        return false;
+        return isEmpty;
     }
 
     static isNumber(fields) {
+        let isNumber = true;
+
         for (let el of fields) {
-            if (!isNaN(parseInt(el)) && isFinite(el)) return false;
+            if (!(!isNaN(parseInt(el.value)) && isFinite(el.value))) {
+                el.setAttribute('required', '');
+                el.setCustomValidity('Поле должно быть числовым!');
+                el.checkValidity();
+
+                isNumber = false;
+            } else {
+                el.setCustomValidity('');
+                el.removeAttribute('required');
+            }
         }
         
-        return true;
+        return isNumber;
+    }
+
+    static isCategory(category) {
+        const conditionCategory = 1 <= +category.value && +category.value <= 6;
+
+        if (!conditionCategory) {
+            category.setAttribute('required', '');
+            category.setCustomValidity('Разряд может быть от 1 до 6!');
+            category.checkValidity();
+        } else {
+            category.setCustomValidity('');
+            category.removeAttribute('required');
+        }
+
+        return conditionCategory;
+    }
+
+    static isAge(age) {
+        const conditionAge = 16 <= +age.value && +age.value <= 90;
+
+        if (!conditionAge) {
+            age.setAttribute('required', '');
+            age.setCustomValidity('Возраст может быть от 16 до 90 лет!');
+            age.checkValidity();
+        } else {
+            age.setCustomValidity('');
+            age.removeAttribute('required');
+        }
+
+        return conditionAge;
     }
 }
 
